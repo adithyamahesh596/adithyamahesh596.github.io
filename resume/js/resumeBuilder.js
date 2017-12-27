85eb35559fa4b4153d23704087b627d73f682ab0
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2017-12-27 12:20:38 -0800

    Resume

--- resume/js/resumeBuilder.js
+++ resume/js/resumeBuilder.js
@@ -62,7 +62,7 @@ var projects = {
             "title" : "LOCATION SEARCH APPLICATION",
             "dates" : "September, 2016 - January, 2017",
             "description" : "<a href='http://locationsearch.cf'>http://locationsearch.cf</a><br>A React based web application which allows users to search places by categories, zip code and radius. Upon querying, the locations are then displayed on a Google Map. <br> <b><u>Responsibilities:</u></b><br>" +
-            "• Built user interfaces by creating re-usable <b>React</b> components. • Created responsive UI using <b>Bootstrap</b>.<br> • Queried and retrieved location places by category and radius using <b>Four Square API</b>. <br>• Displayed locations within a <b>Google Map</b> using Google map markers.<br> • Used <b>Webpack</b> to run build tasks such as bundling and minification. <br> • Deployed and hosted the application using <b>Amazon Web Services (AWS)</b>.<br>" +
+            "• Built user interfaces by creating re-usable <b>React</b> components. <br>• Created responsive UI using <b>Bootstrap</b>.<br> • Queried and retrieved location places by category and radius using <b>Four Square API</b>. <br>• Displayed locations within a <b>Google Map</b> using Google map markers.<br> • Used <b>Webpack</b> to run build tasks such as bundling and minification. <br> • Deployed and hosted the application using <b>Amazon Web Services (AWS)</b>.<br>" +
             "<b><u>Tech stack</u></b>: React, Bootstrap, Webpack, Babel ES6, Node.js, AWS, Google Maps, Four Square API. ",
             "images" : ["https://adithyamahesh596.github.io/images/location.png","https://adithyamahesh596.github.io/images/location2.png"]
         },
